77daf2733cc883fe9b1219aecd5e803985790b2f
Rafael Alves <ralphawk@gmail.com>
2019-10-28 02:36:38 -0300

    Enviar e apagar boletos

--- public/js/gerenciar_boletos.js
+++ public/js/gerenciar_boletos.js
@@ -1,5 +1,8 @@
 var selectAlunos = divPagina.getElementsByTagName("select")[0],
-    listaBoletos = divPagina.getElementsByTagName("ul")[0];
+    listaBoletos = divPagina.getElementsByTagName("ul")[0],
+    formBoletos = divPagina.getElementsByTagName("form")[0],
+    listaEnvio = divPagina.getElementsByTagName("ul")[1],
+    botaoApagar = document.getElementById("botao-apagar");
 
 validarCategoria("admin").then(()=>{
     //criar lista de alunos
@@ -10,19 +13,61 @@ validarCategoria("admin").then(()=>{
     selectAlunos.selectedIndex = 0;
 });
 
+//Listar os boletos do aluno escolhido
 selectAlunos.addEventListener("change", ()=>{
+    formBoletos.setAttribute("hidden","");
     listaBoletos.innerHTML = "Por favor aguarde...";
     storageRef.child(`boletos/${selectAlunos.value}`).listAll().then(lista=>{
         if(lista.items.length == 0){
-            listaBoletos.innerHTML = "Nenhum boleto!"
+            listaBoletos.innerHTML = "Nenhum boleto!";
         } else{
-            let bufer = "";
+            listaBoletos.innerHTML = "";
             lista.items.forEach(boleto=>{
                 storageRef.child(boleto.fullPath).getDownloadURL().then(downloadURL =>{
-                    bufer += `<li><a href="${downloadURL}">${boleto.name}</a></li>`;
+                    listaBoletos.innerHTML += `<li>
+                        <a href="${downloadURL}" target="_blank">${boleto.name}</a> - 
+                        <a href="" class="apagar-boleto" id="${boleto.fullPath}">Apagar</a>
+                    </li>`;
+                    //mostra um modal ao clicar em apagar
+                    document.querySelectorAll(".apagar-boleto").forEach(link=>{
+                        link.addEventListener("click", e=>{
+                            e.preventDefault();
+                            divPagina.querySelector(".modal-body").innerHTML = `Apagar o arquivo ${e.target.id.split("/").pop()}?`;
+                            botaoApagar.dataset.path = e.target.id;
+                            $("#confirmar-remocao-boleto").modal()
+                        });
+                    });
                 });
             });
-            listaBoletos.innerHTML = bufer;
         }
+        formBoletos.removeAttribute("hidden");
     });
+});
+
+//Apagar o boleto após confirmação
+botaoApagar.addEventListener("click", ()=>{
+    storageRef.child(botaoApagar.dataset.path).delete();
+    document.getElementById(botaoApagar.dataset.path).parentElement.remove();
+    if(listaBoletos.innerHTML == ""){
+        listaBoletos.innerHTML = "Nenhum boleto!";
+    }
+});
+
+//Exibe os nomes dos arquivos a serem enviados
+divPagina.getElementsByTagName("input")[0].addEventListener("change", e=>{
+    listaEnvio.innerHTML = "";
+    if(e.target.files.length > 1){
+        Object.values(e.target.files).forEach(arquivo=>{
+            listaEnvio.innerHTML += `<li class="list-inline-item">${arquivo.name}</li>`;
+        });
+    }
+});
+
+//Envio dos arquivos ao armazenamento
+formBoletos.addEventListener("submit", e=>{
+    e.preventDefault();
+    Object.values(e.target[0].files).forEach(arquivo=>{
+        storageRef.child(`boletos/${selectAlunos.value}/${arquivo.name}`).put(arquivo);
+    });
+    paginaSucesso("Envio de boletos realizado!", "gerenciar_boletos");
 });
